84931061b8f030781f1fe3fc6ac00abaf98b7506
Suyashtnt <suyashtnt@gmail.com>
2022-07-22 19:53:56 +0200

    e

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ nix flake update
 
 sudo nixos-rebuild switch --flake .#
 
-home-manager switch --flake .#
+home-manager switch --flake .#$1
 cp users/tntman/config/awesome/* ~/.config/awesome -r # temp fix for issue
 systemctl --user restart picom.service
 popd
