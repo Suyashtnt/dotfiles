1ab567a361ce857fa2c65a686e8e5172d07d5f44
Suyashtnt <suyashtnt@gmail.com>
2022-07-22 16:11:33 +0200

    update git

--- users/tntman/home.nix
+++ users/tntman/home.nix
@@ -142,7 +142,7 @@
         blur = {
           enable = true;
           method = "dual_kawase";
-          strength = 8;
+          strength = 4;
           background = true;
           background-frame = false;
           background-fixed = false;
